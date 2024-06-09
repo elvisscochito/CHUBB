6d8f5757385008347fe2bdd18941ca0b8219cfab
CesarA01424978 <111791586+CesarA01424978@users.noreply.github.com>
2024-06-08 19:18:55 -0600

    Added BrauWeb folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Selection from './pages/Selection'
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Selection />
     </>
   )
 }
---
+++ src/pages/Selection.jsx
@@ -0,0 +1,9 @@
+function Selection() {
+    return (
+        <>
+            <h1>¿En que te encuentras apoyando hoy?</h1>
+        </>
+    )
+}
+
+export default Selection;
